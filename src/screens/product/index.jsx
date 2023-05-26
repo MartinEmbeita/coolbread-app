95b2c47deccf06007d9c8eb3b60aa08893995974
Martin Embeita <martinembeita@hotmail.com>
2023-05-26 17:06:17 -0300

    se integran redux y firebase

--- src/screens/product/index.jsx
+++ src/screens/product/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Button } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import { styles } from "./styles";
-import { PRODUCTS } from "../../constants";
+import { COLORS } from "../../constants";
+import { addToCart } from "../../store/actions";
 
-const Product = ({ route }) => {
-  const { productId } = route.params;
+const Product = () => {
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.products.selected);
 
-  const product = PRODUCTS.find((product) => product.id === productId);
+  const onAddToCart = () => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <View style={styles.container}>
       <Image resizeMode="contain" source={{ uri: product.image }} style={styles.image} />
@@ -15,6 +21,7 @@ const Product = ({ route }) => {
       <Text style={styles.description}>{product.description}</Text>
       <Text style={styles.price}>USD {product.price}</Text>
       <Text style={styles.weight}>weight: {product.weight}</Text>
+      <Button title="Add to cart" onPress={onAddToCart} color={COLORS.text} />
     </View>
   );
 };
